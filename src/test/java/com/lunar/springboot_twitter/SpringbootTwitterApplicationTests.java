34c92bcebcca1bc7017539125f6d9081d38c3db5
uoahns23 <uoow.23@gmail.com>
2026-04-10 00:51:37 +0900

    Ignore springboottest

--- src/test/java/com/lunar/springboot_twitter/SpringbootTwitterApplicationTests.java
+++ src/test/java/com/lunar/springboot_twitter/SpringbootTwitterApplicationTests.java
@@ -1,8 +1,10 @@
 package com.lunar.springboot_twitter;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class SpringbootTwitterApplicationTests {
 
